9b85f896f3f6f722d2d3458af0620c61b82d66ea
sfun <jiegto@outlook.com>
2025-09-19 22:05:18 +0800

    Update alfred.go

--- github-control-workflow/alfred.go
+++ github-control-workflow/alfred.go
@@ -1,12 +1,13 @@
 package main
 
 type AlfredItem struct {
-	Title    string               `json:"title"`
-	Subtitle string               `json:"subtitle"`
-	Arg      string               `json:"arg,omitempty"`
-	Valid    bool                 `json:"valid"`
-	Match    string               `json:"match,omitempty"`
-	Mods     map[string]AlfredMod `json:"mods,omitempty"`
+	Title      string               `json:"title"`
+	Subtitle   string               `json:"subtitle"`
+	Arg        string               `json:"arg,omitempty"`
+	Valid      bool                 `json:"valid"`
+	Match      string               `json:"match,omitempty"`
+	Mods       map[string]AlfredMod `json:"mods,omitempty"`
+	Variables  map[string]string    `json:"variables,omitempty"`
 }
 
 type AlfredMod struct {
