b8e7f36db4bd8b0ea38fa32cdd5838bb4f1fa797
sfun <jiegto@outlook.com>
2025-09-26 14:22:42 +0800

    Update main.go

--- calculate-anything/main.go
+++ calculate-anything/main.go
@@ -1,18 +1,23 @@
+// calculate-anything-go/main.go
 package main
 
 import (
-	"github.com/deanishe/awgo"
-	"calculate-anything-go/cmd"
+    "calculate-anything-go/cmd"
+    "github.com/deanishe/awgo"
 )
 
+// wf 是一个全局的 Workflow 实例
 var wf *aw.Workflow
 
 func init() {
-	wf = aw.New()
+    // 初始化一个新的 Workflow 对象
+    wf = aw.New(aw.HelpURL("https://github.com/biati-digital/alfred-calculate-anything"), aw.Update(aw.GitHub("biati-digital/alfred-calculate-anything")))
 }
 
 func main() {
-	// 将实际的业务逻辑封装在 cmd.Run 中
-	// awgo 会自动处理 panic 并向 Alfred 显示错误
-	wf.Run(cmd.Run)
+    // 将实际的业务逻辑传递给 wf.Run
+    // awgo 会处理查询参数、panic 恢复和向 Alfred 发送反馈
+    wf.Run(func() {
+        cmd.Run(wf)
+    })
 }
