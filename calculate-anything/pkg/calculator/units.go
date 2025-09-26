9f58ca656bf655c5a61dc16481b858d8b74d6d4b
sfun <jiegto@outlook.com>
2025-09-26 14:49:00 +0800

    Update units.go

--- calculate-anything/pkg/calculator/units.go
+++ calculate-anything/pkg/calculator/units.go
@@ -1,9 +1,9 @@
-// calculate-anything-go/pkg/calculators/units.go
+// calculate-anything/pkg/calculators/units.go
 package calculators
 
 import (
-	"calculate-anything-go/pkg/alfred"
-	"calculate-anything-go/pkg/parser"
+	"calculate-anything/pkg/alfred"
+	"calculate-anything/pkg/parser"
 	"fmt"
 	"github.com/deanishe/awgo"
 	"strconv"
