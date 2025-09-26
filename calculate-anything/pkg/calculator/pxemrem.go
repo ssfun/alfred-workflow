f7203ed23f19fa7b4e666ac6059f5b716d5c27f0
sfun <jiegto@outlook.com>
2025-09-26 14:48:34 +0800

    Update pxemrem.go

--- calculate-anything/pkg/calculator/pxemrem.go
+++ calculate-anything/pkg/calculator/pxemrem.go
@@ -1,10 +1,10 @@
-// calculate-anything-go/pkg/calculators/pxemrem.go
+// calculate-anything/pkg/calculators/pxemrem.go
 package calculators
 
 import (
-	"calculate-anything-go/pkg/alfred"
-	"calculate-anything-go/pkg/config"
-	"calculate-anything-go/pkg/parser"
+	"calculate-anything/pkg/alfred"
+	"calculate-anything/pkg/config"
+	"calculate-anything/pkg/parser"
 	"fmt"
 	"github.com/deanishe/awgo"
 	"strconv"
