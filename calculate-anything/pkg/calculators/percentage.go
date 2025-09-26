812c71334278a73998c897b70c010e70167b639d
sfun <jiegto@outlook.com>
2025-09-26 15:20:19 +0800

    Update percentage.go

--- calculate-anything/pkg/calculators/percentage.go
+++ calculate-anything/pkg/calculators/percentage.go
@@ -2,43 +2,56 @@
 package calculators
 
 import (
+	"calculate-anything/pkg/alfred"
 	"calculate-anything/pkg/parser"
 	"fmt"
+	"github.com/deanishe/awgo"
 )
 
+// HandlePercentage 处理所有类型的百分比计算。
 func HandlePercentage(wf *aw.Workflow, p *parser.ParsedQuery) {
 	var result float64
-    var title, arg string
+	var title, arg string
 
+	// 根据解析器识别出的不同动作，执行相应的计算
 	switch p.Action {
+	// 场景 1: "120 + 30%"
 	case "+":
 		result = p.BaseValue * (1 + p.Percent/100)
-        title = fmt.Sprintf("%.2f + %.2f%% = %.2f", p.BaseValue, p.Percent, result)
-        arg = fmt.Sprintf("%.2f", result)
+		title = fmt.Sprintf("%g + %g%% = %g", p.BaseValue, p.Percent, result)
+		arg = fmt.Sprintf("%g", result)
+
+	// 场景 2: "120 - 30%"
 	case "-":
 		result = p.BaseValue * (1 - p.Percent/100)
-        title = fmt.Sprintf("%.2f - %.2f%% = %.2f", p.BaseValue, p.Percent, result)
-        arg = fmt.Sprintf("%.2f", result)
-    case "of":
-        result = (p.Percent / 100) * p.BaseValue
-        title = fmt.Sprintf("%.2f%% of %.2f = %.2f", p.Percent, p.BaseValue, result)
-        arg = fmt.Sprintf("%.2f", result)
-    case "as % of":
-        if p.BaseValue == 0 {
-            alfred.ShowError(wf, fmt.Errorf("不能计算 0 的百分比"))
-            return
-        }
-        result = (p.Amount / p.BaseValue) * 100
-        title = fmt.Sprintf("%g 是 %g 的 %.2f%%", p.Amount, p.BaseValue, result)
-        arg = fmt.Sprintf("%.2f", result)
+		title = fmt.Sprintf("%g - %g%% = %g", p.BaseValue, p.Percent, result)
+		arg = fmt.Sprintf("%g", result)
+
+	// 场景 3: "15% of 50"
+	case "of":
+		result = (p.Percent / 100) * p.BaseValue
+		title = fmt.Sprintf("%g%% of %g = %g", p.Percent, p.BaseValue, result)
+		arg = fmt.Sprintf("%g", result)
+
+	// 场景 4: "40 as a % of 50"
+	case "as % of":
+		if p.BaseValue == 0 {
+			alfred.ShowError(wf, fmt.Errorf("不能计算 0 的百分比"))
+			return
+		}
+		result = (p.Amount / p.BaseValue) * 100
+		title = fmt.Sprintf("%g 是 %g 的 %g%%", p.Amount, p.BaseValue, result)
+		arg = fmt.Sprintf("%g", result)
+
 	default:
 		alfred.ShowError(wf, fmt.Errorf("未知的百分比操作: %s", p.Action))
-        return
+		return
 	}
-	
-    alfred.AddToWorkflow(wf, []alfred.Result{{
-        Title: title,
-        Subtitle: fmt.Sprintf("复制 '%s'", arg),
-        Arg: arg,
-    }})
+
+	// 将计算结果添加到 Alfred 反馈
+	alfred.AddToWorkflow(wf, []alfred.Result{{
+		Title:    title,
+		Subtitle: fmt.Sprintf("复制 '%s'", arg),
+		Arg:      arg,
+	}})
 }
